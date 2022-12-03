ca69b02d2cdb65f2a1dc6cdb8b5c1c722f90b85e
Liam Ozog <liam.ozog@gmail.com>
2022-12-03 14:06:05 -0500

    day 03 p2

--- 03/main.go
+++ 03/main.go
@@ -6,16 +6,36 @@ import (
 	"os"
 )
 
-// assumes len(first) == len(second)
+// finds a shared element between two strings
 func findSharedElement(first string, second string) rune {
-	for _, firstElement := range first {
-		for _, secondElement := range second {
+	longerString := first
+	shorterString := second
+	if len(second) > len(first) {
+		longerString = second
+		shorterString = first
+	}
+
+	for _, firstElement := range shorterString {
+		for _, secondElement := range longerString {
 			if firstElement == secondElement {
 				return firstElement
 			}
 		}
 	}
 	panic("could not find shared element")
+}
+
+// finds all shared elements between two strings
+func findSharedElements(first string, second string) string {
+	results := ""
+	for _, firstElement := range first {
+		for _, secondElement := range second {
+			if firstElement == secondElement {
+				results += string(firstElement)
+			}
+		}
+	}
+	return results
 }
 
 // a == 97
@@ -35,7 +55,7 @@ func runeToPriority(element rune) int {
 	}
 }
 
-func main() {
+func p1() {
 	readFile, _ := os.Open("data.txt")
 	fileScanner := bufio.NewScanner(readFile)
 	fileScanner.Split(bufio.ScanLines)
@@ -62,4 +82,48 @@ func main() {
 	readFile.Close()
 
 	fmt.Printf("p1: %d\n", sum)
+}
+
+func p2() {
+	readFile, _ := os.Open("data.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	sum := 0
+	var elvesInGroup []string
+
+	i := 0
+	for fileScanner.Scan() {
+		i += 1
+		line := fileScanner.Text()
+		// fmt.Println(line)
+
+		elvesInGroup = append(elvesInGroup, line)
+
+		if i%3 != 0 {
+			// keep going until we get to each third elf
+			continue
+		}
+
+		// find the element that is shared in first two elves
+		sharedElementFirstTwo := findSharedElements(elvesInGroup[0], elvesInGroup[1])
+
+		// find element shared between all three
+		sharedElement := findSharedElement(sharedElementFirstTwo, elvesInGroup[2])
+
+		// get that element's priority
+		priority := runeToPriority(sharedElement)
+
+		// add it to the sum
+		sum += priority
+		elvesInGroup = nil
+	}
+	readFile.Close()
+
+	fmt.Printf("p2: %d\n", sum)
+}
+
+func main() {
+	p1()
+	p2()
 }
