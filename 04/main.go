68e2ffd07113b33f050c62863ba7aeaa98c19577
Liam Ozog <liam.ozog@gmail.com>
2022-12-04 23:46:27 -0500

    day 04 p2

--- 04/main.go
+++ 04/main.go
@@ -51,6 +51,38 @@ func p1() {
 	fmt.Printf("p1 result: %d\n", sum)
 }
 
+func p2() {
+	readFile, _ := os.Open("data.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	sum := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		pairs := strings.Split(line, ",")
+
+		pair1 := stringSliceToInt(strings.Split(pairs[0], "-"))
+		pair2 := stringSliceToInt(strings.Split(pairs[1], "-"))
+
+		// fmt.Println(line)
+
+		if pair1[0] <= pair2[0] && pair2[0] <= pair1[1] {
+			// if pair1 comes first and pair2 starts before pair1 ends
+			// fmt.Printf("%d <= %d && %d <= %d\n", pair1[0], pair2[0], pair2[0], pair1[1])
+			sum += 1
+		} else if pair2[0] <= pair1[0] && pair1[0] <= pair2[1] {
+			// if pair2 comes first and pair1 starts before pair2 ends
+			// fmt.Printf("%d <= %d && %d <= %d\n", pair2[0], pair1[0], pair1[0], pair2[1])
+			sum += 1
+		}
+	}
+	readFile.Close()
+
+	fmt.Printf("p1 result: %d\n", sum)
+}
+
 func main() {
-	p1()
+	// p1()
+	p2()
 }
