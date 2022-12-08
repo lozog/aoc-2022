b6b89f983092fd4ccad61e890bf7aad3626d75ed
Liam Ozog <liam.ozog@gmail.com>
2022-12-08 18:53:54 -0500

    day 07 p1

---
+++ 07/go.mod
@@ -0,0 +1,5 @@
+module 07
+
+go 1.19
+
+require go.yhsif.com/defaultdict v0.2.1 // indirect
---
+++ 07/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func changeDirectory(path []string, dstDir string) []string {
+
+	if dstDir == ".." {
+		if len(path) == 1 {
+			// if root, stay on the root
+			return path
+		}
+		// handle moving up a folder
+		return path[:len(path)-1]
+	}
+
+	return append(path, dstDir)
+}
+
+// [/ foo bar baz] => "/foo/bar/baz"
+func dirSliceToString(path []string) string {
+	if len(path) == 1 {
+		return path[0]
+	}
+	res := strings.Join(path[1:], "/")
+	return path[0] + res
+}
+
+func solution() {
+	readFile, _ := os.Open("data.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	// curDir := ""
+	curDir := []string{}
+	curCmd := ""
+	folderSizes := make(map[string]int)
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		fmt.Printf("%s\n", line)
+		splitLine := strings.Split(line, " ")
+
+		if splitLine[0] == "$" {
+			curCmd = splitLine[1]
+
+			if curCmd == "cd" {
+				dstDir := splitLine[2]
+				curDir = changeDirectory(curDir, dstDir)
+				fmt.Printf("changing to new dir: %s\n", curDir)
+			}
+		} else if curCmd == "ls" {
+			if splitLine[0] == "dir" {
+				// curDir = changeDirectory(curDir, splitLine[1])
+				// curDirAsString := dirSliceToString(curDir)
+				// folderSizes[curDirAsString] = 0
+				// fmt.Printf("changing to new dir: %s\n", curDir)
+			} else {
+				// record file sizes
+				res, _ := strconv.Atoi(splitLine[0])
+				// curDirAsString := dirSliceToString(curDir)
+				// folderSizes[curDirAsString] += res
+
+				// also add filesize to every parent folder
+				dir := curDir
+				for dirSliceToString(dir) != "/" {
+					folderSizes[dirSliceToString(dir)] += res
+					// fmt.Printf("adding %d to %s: %d\n", res, dirSliceToString(dir), folderSizes[dirSliceToString(dir)])
+					dir = changeDirectory(dir, "..")
+				}
+				// finally, add it to the root
+				folderSizes["/"] += res
+				// fmt.Printf("adding %d to %s: %d\n", res, "/", folderSizes["/"])
+
+			}
+		}
+	}
+	readFile.Close()
+
+	// fmt.Printf("%+v\n", folderSizes)
+
+	sum := 0
+	for _, fileSize := range folderSizes {
+		if fileSize <= 100000 {
+			sum += fileSize
+		}
+	}
+	fmt.Printf("p1: %d\n", sum)
+
+}
+
+func main() {
+	solution()
+}
