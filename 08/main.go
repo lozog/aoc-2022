fed910380c6bf47bc7b6bc5bebea81a81c2a4873
Liam Ozog <liam.ozog@gmail.com>
2022-12-09 10:43:31 -0500

    day 08 p2

--- 08/main.go
+++ 08/main.go
@@ -59,7 +59,7 @@ func solution() {
 	readFile.Close()
 
 	// for row := 0; row < len(treeHeightMap); row++ {
-	// 	fmt.Printf("%t\n", treeVisibilityMap[row])
+	// 	fmt.Printf("%d\n", treeHeightMap[row])
 	// }
 
 	for col := 0; col < len(treeHeightMap[0]); col++ {
@@ -82,16 +82,69 @@ func solution() {
 		}
 	}
 
+	height := len(treeHeightMap)
+	width := len(treeHeightMap[0])
+
 	sum := 0
-	for row := 0; row < len(treeHeightMap); row++ {
+	for row := 0; row < height; row++ {
 		// fmt.Printf("%t\n", treeVisibilityMap[row])
-		for col := 0; col < len(treeHeightMap[0]); col++ {
+		for col := 0; col < width; col++ {
 			if treeVisibilityMap[row][col] {
 				sum += 1
 			}
 		}
 	}
 	fmt.Printf("p1: %d\n", sum)
+
+	// part 2:
+	// from every tree, look in every direction and calculate its scenic score
+	maxScenicScore := 0
+	for row := 0; row < height; row++ {
+		// fmt.Printf("%t\n", treeVisibilityMap[row])
+		for col := 0; col < width; col++ {
+			scenicScoreParts := [4]int{0, 0, 0, 0}
+
+			// look right
+			for y := col + 1; y < width; y++ {
+				scenicScoreParts[0] += 1
+				if treeHeightMap[row][y] >= treeHeightMap[row][col] {
+					break
+				}
+			}
+
+			// look left
+			for y := col - 1; y >= 0; y-- {
+				scenicScoreParts[1] += 1
+				if treeHeightMap[row][y] >= treeHeightMap[row][col] {
+					break
+				}
+			}
+
+			// look down
+			for x := row + 1; x < height; x++ {
+				scenicScoreParts[2] += 1
+				if treeHeightMap[x][col] >= treeHeightMap[row][col] {
+					break
+				}
+			}
+
+			// look up
+			for x := row - 1; x >= 0; x-- {
+				scenicScoreParts[3] += 1
+				if treeHeightMap[x][col] >= treeHeightMap[row][col] {
+					break
+				}
+			}
+
+			// fmt.Println(scenicScoreParts)
+
+			scenicScore := funk.Reduce(scenicScoreParts, '*', 1).(int)
+			if scenicScore > maxScenicScore {
+				maxScenicScore = scenicScore
+			}
+		}
+	}
+	fmt.Printf("p2: %d\n", maxScenicScore)
 }
 
 func main() {
