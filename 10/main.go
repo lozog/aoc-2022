de6ec234c4dbff7eba0a99e8882c8c5e782ad631
Liam Ozog <liam.ozog@gmail.com>
2022-12-10 18:07:29 -0500

    day 10 p2

--- 10/main.go
+++ 10/main.go
@@ -8,20 +8,6 @@ import (
 	"strings"
 )
 
-func incrementCycles(startCycle, cyclesToRun int, X int) (int, int) {
-	cycle := startCycle
-	signalStrength := 0
-	for i := 0; i < cyclesToRun; i++ {
-		// fmt.Printf("during cycle %d, X=%d\n", cycle, X)
-		if (cycle+20)%40 == 0 {
-			signalStrength = X * cycle
-			// fmt.Printf("signal at cycle %d: %d\n", cycle, signalStrength)
-		}
-		cycle += 1
-	}
-	return cycle, signalStrength
-}
-
 func solution() {
 	readFile, _ := os.Open("data.txt")
 	fileScanner := bufio.NewScanner(readFile)
@@ -30,7 +16,9 @@ func solution() {
 	X := 1
 	cycle := 1
 	sum := 0
-	signalStrength := 0
+	display := [][]rune{}
+	// display = append(display, []rune{})
+	curRow := -1
 
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
@@ -38,20 +26,52 @@ func solution() {
 		lineSplit := strings.Split(line, " ")
 		instruction := lineSplit[0]
 		var argument int
+		cyclesToRun := 1
 		if len(lineSplit) > 1 {
 			argument, _ = strconv.Atoi(lineSplit[1])
+			cyclesToRun = 2
 		}
 
-		if instruction == "noop" {
-			cycle, signalStrength = incrementCycles(cycle, 1, X)
-		} else if instruction == "addx" {
-			cycle, signalStrength = incrementCycles(cycle, 2, X)
+		signalStrength := 0
+		for i := 0; i < cyclesToRun; i++ {
+			// fmt.Printf("signal at cycle %d: %d\n", cycle, X)
+
+			// p1
+			if (cycle+20)%40 == 0 {
+				signalStrength = X * cycle
+				// fmt.Printf("signal at cycle %d: %d\n", cycle, signalStrength)
+			}
+
+			curPixelPos := (cycle - 1) % 40
+			if curPixelPos == 0 {
+				display = append(display, []rune{})
+				curRow += 1
+			}
+
+			elementToDraw := '.'
+			if X-1 <= curPixelPos && curPixelPos <= X+1 {
+				elementToDraw = '#'
+			}
+
+			// fmt.Printf("drawing %s for cycle %d\n", string(elementToDraw), cycle)
+			display[curRow] = append(display[curRow], elementToDraw)
+
+			cycle += 1
+		}
+		if instruction == "addx" {
 			X += argument
 		}
 		sum += signalStrength
 	}
 	readFile.Close()
-	fmt.Printf("solution: %d\n", sum)
+	fmt.Printf("p1: %d\n", sum)
+	fmt.Print("p2:\n")
+	for x := 0; x < len(display); x++ {
+		for y := 0; y < len(display[x]); y++ {
+			fmt.Printf("%s", string(display[x][y]))
+		}
+		fmt.Printf("\n")
+	}
 }
 
 func main() {
